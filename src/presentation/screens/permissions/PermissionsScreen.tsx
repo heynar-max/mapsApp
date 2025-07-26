0661752f38b3a9724e0f7f548a02abbf90a0dd66
heynar-max <heynar76@hotmail.com>
2025-07-26 17:07:59 -0500

    Consumir nuestro Store

--- src/presentation/screens/permissions/PermissionsScreen.tsx
+++ src/presentation/screens/permissions/PermissionsScreen.tsx
@@ -1,10 +1,46 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Pressable, StyleSheet, Text, View } from 'react-native'
+import { globalStyles } from '../../../config/theme/styles'
+import { usePermissionStore } from '../../store/permissions/usePermissionStore';
 
 export const PermissionsScreen = () => {
+
+    const { locationStatus, requestLocationPermission } = usePermissionStore();
+
     return (
-        <View>
-            <Text>LoadingScreen</Text>
+        <View style={styles.container}>
+            <Text style={styles.title}>Habilitar ubicación</Text>
+
+            <Pressable
+                style={ globalStyles.btnPrimary }
+                onPress={ requestLocationPermission }
+            >
+                <Text style={styles.buttonText}>Habilitar Localización</Text>
+            </Pressable>
+
+
+            <Text style={styles.statusText}>Estado actual: { locationStatus }</Text>
+            
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    title: {
+        fontSize: 18,
+        marginBottom: 20,
+    },
+    buttonText: {
+        color: 'white',
+        fontWeight: 'bold',
+    },
+    statusText: {
+        marginTop: 20,
+        fontSize: 16,
+    },
+});
