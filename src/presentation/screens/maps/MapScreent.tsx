fb445d4b678d85b067da77124b4b778b12a8d30e
heynar-max <heynar76@hotmail.com>
2025-07-26 14:19:35 -0500

    inicion del proyecto

---
+++ src/presentation/screens/maps/MapScreent.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export const MapScreent = () => {
+    return (
+        <View>
+            <Text>LoadingScreen</Text>
+        </View>
+    )
+}
