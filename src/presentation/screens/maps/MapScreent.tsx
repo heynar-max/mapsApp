2dacf3fdbe76b05362a702858e1549afc625d4f2
heynar-max <heynar76@hotmail.com>
2025-07-31 18:19:19 -0500

    Store - Información de la ubicación

--- src/presentation/screens/maps/MapScreent.tsx
+++ src/presentation/screens/maps/MapScreent.tsx
@@ -1,11 +1,28 @@
 import { StyleSheet,  View } from 'react-native'
 import { Map } from '../../components/maps/Map';
+import { useLocationStore } from '../../store/location/useLocationStore';
+import { useEffect } from 'react';
+import { LoadingScreen } from '../loading/LoadingScreen';
 
 
 export const MapScreent = () => {
+
+    const { lastKnownLocation, getLocation} = useLocationStore();
+
+    useEffect(() => {
+        if (lastKnownLocation === null){
+            getLocation();
+        }
+    }, [getLocation,lastKnownLocation])
+    
+    if (lastKnownLocation === null ){
+        return (<LoadingScreen/>)
+    }
     return (
         <View style={styles.container}>
-            {/* <Map/> */}
+            <Map
+            initialLocation={ lastKnownLocation}
+            />
         </View>
     )
 }
