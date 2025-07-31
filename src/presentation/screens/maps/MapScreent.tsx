ecbdca73780a1be71980896437def68a84b1110c
heynar-max <heynar76@hotmail.com>
2025-07-31 17:33:20 -0500

    Marcadores y componente Mapa re-utilizable

--- src/presentation/screens/maps/MapScreent.tsx
+++ src/presentation/screens/maps/MapScreent.tsx
@@ -1,30 +1,17 @@
-import React from 'react'
 import { StyleSheet,  View } from 'react-native'
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import { Map } from '../../components/maps/Map';
+
 
 export const MapScreent = () => {
     return (
         <View style={styles.container}>
-            <MapView
-            provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-            style={styles.map}
-            region={{
-                latitude: 37.78825,
-                longitude: -122.4324,
-                latitudeDelta: 0.015,
-                longitudeDelta: 0.0121,
-            }}
-            >
-            </MapView>
+            <Map/>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-    ...StyleSheet.absoluteFillObject,
-    },
-    map: {
     ...StyleSheet.absoluteFillObject,
     },
 });
