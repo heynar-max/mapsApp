17eaa09aa20227f3900eecab63d1ac299a76065c
heynar-max <heynar76@hotmail.com>
2025-07-31 19:37:41 -0500

    Seguimiento de usuario - Store

--- src/presentation/store/location/useLocationStore.tsx
+++ src/presentation/store/location/useLocationStore.tsx
@@ -1,20 +1,28 @@
 import { create } from 'zustand';
 import { Location } from '../../../infrastructure/interfaces/location';
-import {  getCurrentLocation } from '../../../actions/location/location';
+import {  clearWatchLocation, getCurrentLocation, watchCurrentLocation } from '../../../actions/location/location';
 
 
 
 interface LocationState {
     lastKnownLocation: Location | null;
-    
+    userLocationList: Location[];
+    watchId: number | null;
+
+
+
     getLocation: ()=> Promise<Location|null>;
+    watchLocation: ()=> void;
+    clearWatchLocation: ()=> void;
     
 }
 
-export const useLocationStore = create<LocationState>()( (set) => ({
+export const useLocationStore = create<LocationState>()( (set, get) => ({
 
     lastKnownLocation: null,
-    
+    userLocationList: [],
+    watchId: null,
+
 
     getLocation: async() => {
 
@@ -22,5 +30,33 @@ export const useLocationStore = create<LocationState>()( (set) => ({
         set({ lastKnownLocation: location });
         return location;
     },
+
+
+    watchLocation: () => {
+        const watchId = get().watchId;
+        if ( watchId !== null ) {
+        get().clearWatchLocation();
+        }
+
+        const id = watchCurrentLocation( (location) => {
+        set({
+            lastKnownLocation: location,
+            userLocationList: [...get().userLocationList, location]
+        })
+
+        });
+
+        set({ watchId: id });
+
+
+    },
+
+    clearWatchLocation: () => {
+        const watchId = get().watchId;
+        if ( watchId !== null ) {
+        clearWatchLocation(watchId);
+        }
+    }
+
 
 }))
